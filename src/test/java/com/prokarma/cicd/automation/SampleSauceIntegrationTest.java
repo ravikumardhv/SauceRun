22058b84fe7065df5542b01c11f9a34acd80251e
ravikumar <java4u1983@gmail.com>
2016-01-25 14:47:15 -0700

    key added

--- src/test/java/com/prokarma/cicd/automation/SampleSauceIntegrationTest.java
+++ src/test/java/com/prokarma/cicd/automation/SampleSauceIntegrationTest.java
@@ -41,7 +41,7 @@ public class SampleSauceIntegrationTest implements SauceOnDemandSessionIdProvide
      * Constructs a {@link com.saucelabs.common.SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
      * supplied by environment variables or from an external file, use the no-arg {@link com.saucelabs.common.SauceOnDemandAuthentication} constructor.
      */
-    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("kumar__donthamsetti", "61f46ea9-3481-47d7-bd83-ad3ace8efc41");
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("kumar__donthamsetti", "ec3aa3eb-ea85-4c4c-9577-9b986b500477");
 
     /**
      * ThreadLocal variable which contains the  {@link WebDriver} instance which is used to perform browser interactions with.
